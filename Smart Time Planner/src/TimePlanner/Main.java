129ea0667397a672ad6e6b319c2891df6621eeb1
Oussama Abderraouf Attia <92870205+OussamaAbderraoufAttia@users.noreply.github.com>
2023-05-19 17:32:08 +0100

    changes are made and pngs are added!

--- Smart Time Planner/src/TimePlanner/Main.java
+++ Smart Time Planner/src/TimePlanner/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Frontend/Pages/Profile/Profile.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Frontend/Pages/Login/Login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1228, 740));
         primaryStage.show();

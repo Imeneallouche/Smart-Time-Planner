af82b8af2a049b24a599ffa4b33860f216f5df6a
Imène ALLOUCJE <https://github.com/Imeneallouche>
2023-05-21 17:05:31 +0100

    calendar page frontend has been settled(not the backedn yet)

--- Smart Time Planner/src/TimePlanner/Main.java
+++ Smart Time Planner/src/TimePlanner/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Frontend/Pages/CreneauxLibres/CreneauxLibres.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Frontend/Pages/Calendar/TaskCalendar.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
